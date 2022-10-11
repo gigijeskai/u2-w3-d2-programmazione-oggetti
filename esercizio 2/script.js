cf82914ec2bb64898ba5bc9489e9ce46ae807e65
Luigi Indaco <luigindaco@gmail.com>
2022-10-11 16:44:10 +0200

    consegna

--- esercizio 2/script.js
+++ esercizio 2/script.js
@@ -1,25 +1,36 @@
+// class pagina {
+//   items = [];
+//   pageSize = [];
+
+//   addInArray(persona) {}
+// }
+
 class Persona {
   constructor(nome, cognome, anni) {
     this.nome = nome;
     this.cognome = cognome;
     this.anni = anni;
   }
-  createTablePersone() {
-    let table = document.createElement("table");
-    let tr = document.createElement("tr");
-    table.appendChild(tr);
-    let td = document.createElement("td");
-    tr.appendChild(td);
-    td.innerHTML += this.nome;
-    td.innerHTML += this.cognome;
-    td.innerHTML += this.anni;
+  createListPersone() {
+    let ul = document.createElement("ul");
+    let li = document.createElement("li");
+    ul.appendChild(li);
+    li.innerHTML += this.nome;
+    li.innerHTML += this.cognome;
+    li.innerHTML += this.anni;
 
     let persone = document.querySelector("#persone");
-    persone.appendChild(table);
+    persone.appendChild(ul);
   }
+
+  // addInArray() {
+  //   let item = [];
+  //   let pageSize = [];
+
+  // }
 }
 
 function aggiungiPersona() {
   let persone = new Persona(document.querySelector("#nome").value, document.querySelector("#cognome").value, document.querySelector("#anni").value);
-  persone.createTablePersone();
+  persone.createListPersone();
 }
